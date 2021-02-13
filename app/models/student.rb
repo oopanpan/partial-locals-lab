c5567f27bff3167d8b34569ce83d68f65eb79046
oopanpan <oopanpan@gmail.com>
2021-02-12 23:17:15 -0800

    commit tree cd72bb7ef5b53cc3ed667010a0d779975852fb70

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,11 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  # def search(string)
+  #   if string == ''
+  #     Student.all
+  #   elsif 
+  #   end
+  # end
 end
